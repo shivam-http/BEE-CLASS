b83514818b914be5371e90f66f48e00536213d52
unknown <shivamjaswal2550@gmail.com>
2025-02-27 14:43:44 +0530

    Middleware to Parse urlencoded data

--- SignUp/server.js
+++ SignUp/server.js
@@ -1,13 +1,23 @@
 const express=require ("express")
 const app =express();
+//middleware to parse urlencoded data
+app.use(express.urlencoded({extended:true}))
 app.get("/signup",(req,res)=>{
     res.sendFile(__dirname+"/signup.html")  //__dirname set the current directory
 })
 
-
+let userData=[];
 app.post("/signup",(req,res)=>{
     let {username,useremail,userpassword}=req.body;
     console.log(username,useremail,userpassword);
+    let newUser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userData.push(newUser);
+    console.log(username,useremail,userpassword)
+    res.send(userData);
 })
 
 
